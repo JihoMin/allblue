2bf3c2e6b2692c845ea55b4247a40b2fd8bd9189
shinjh0849 <21200421@handong.edu>
2018-06-09 00:01:47 +0900

    my sales update

--- src/app/ui-allblue/my-sales/my-sales.component.ts
+++ src/app/ui-allblue/my-sales/my-sales.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
-import { Observable } from 'rxjs';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { AuthService } from '../../core/auth.service';
+import { Sales } from '../../Sales';
+import { Observable, Subscription, BehaviorSubject, Subject } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'my-sales',
@@ -10,28 +12,52 @@ import { AuthService } from '../../core/auth.service';
 })
 
 export class MySalesComponent implements OnInit {
-  my_sales: Observable<any[]>;
-  userID: any;
-  salesCollection : AngularFirestoreCollection<any>
-
+  saleSnapShot: any;
+  private saleCollection: AngularFirestoreCollection<Sales>;
+  userID: string;
+  salesCollection: AngularFirestoreCollection<any>
+  my_sales: Sales[] = [];
 
   constructor(public afstore: AngularFirestore, private auth: AuthService) {
+
     auth.user.subscribe(doc => {
-      this.my_sales = afstore.collection('sales', ref => ref.where('userID', '==', doc.uid)).valueChanges();
-      afstore.collection('sales', ref => ref.where('userID', '==', doc.uid)).snapshotChanges().subscribe(actions =>{
-        actions.map( a => {
-          let data = a.payload.doc.data() as any;
-          data.id = a.payload.doc.id;
-            this.userID = data;        
-        });
-      });
+      this.userID = doc.uid;
+      // this.salesCollection = afstore.collection('sales', ref => ref.where('userID', '==', doc.uid));
+
+      // this.saleSnapShot = this.saleCollection.snapshotChanges().pipe(
+      //   map(actions => actions.map(a => {
+      //     const data = a.payload.doc.data() as Sales;
+      //     const id = a.payload.doc.id;
+      //     return { id, data };
+      //   }))
+      // ).subscribe(S => {
+      //   this.my_sales = S;
+      // });
+
+      let self = this;
+      self.saleSnapShot = self.afstore.collection(`sales`, ref => ref
+        .orderBy("userID")
+        .startAt(self.userID)
+        .endAt(self.userID + "\uf8ff")
+        .limit(10))
+        .snapshotChanges().pipe(
+          map(actions => actions.map(a => {
+            const data = a.payload.doc.data() as Sales;
+            const id = a.payload.doc.id;
+            return { data, id };
+          }))
+        ).subscribe(S => {
+          this.my_sales = S;
+        }
+        )
+
+
     });
+
   }
 
   ngOnInit() {
-    console.log("my-sales loaded, uID: " + this.userID);
 
-    
   }
 }
 
--- src/app/ui-allblue/sale-confirm/sale-confirm.component.ts
+++ src/app/ui-allblue/sale-confirm/sale-confirm.component.ts
@@ -16,11 +16,11 @@ export class SaleConfirmComponent implements OnInit {
   joiners: Observable<any[]>;
   displayedColumns = ['position', 'name', 'accepted', 'action'];
   //dataSource = new MatTableDataSource(ELEMENT_DATA);
-  id: string;
+  salesID: string;
   constructor(public afstore: AngularFirestore,private route: ActivatedRoute, private auth: AuthService){
-    this.id = this.route.snapshot.paramMap.get('id');
+    this.salesID = this.route.snapshot.paramMap.get('id');
     this.auth.user.subscribe(doc => {
-      this.joiners = afstore.collection(doc.uid).doc(this.id).collection('joiner').valueChanges();
+      this.joiners = afstore.collection(doc.uid).doc(this.salesID).collection('joiner').valueChanges();
     })
     
   }
@@ -35,22 +35,3 @@ export class SaleConfirmComponent implements OnInit {
   }
 
 }
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  accepted: string;
-}
-
-// const ELEMENT_DATA: PeriodicElement[] = [
-//   { position: 1, name: '신지호', phonenumber: '010-9788-0849', accepted: 'accepted' },
-//   { position: 2, name: '원은지', phonenumber: '010-123-4567', accepted: 'accepted' },
-//   { position: 3, name: '민지호', phonenumber: '010-123-4657', accepted: 'accepted' },
-//   { position: 4, name: '권지혜', phonenumber: '010-123-4567', accepted: 'accepted' },
-//   { position: 5, name: '강현우', phonenumber: '010-123-4561', accepted: 'accepted' },
-//   { position: 6, name: '최민우', phonenumber: '010-123-4567', accepted: 'unconfirmed' },
-//   { position: 7, name: '문세미', phonenumber: '010-123-4567', accepted: 'unconfirmed' },
-//   { position: 8, name: '김응가', phonenumber: '010-123-4567', accepted: 'unconfirmed' },
-//   { position: 9, name: '권소영', phonenumber: '010-123-4567', accepted: 'rejected' },
-//   { position: 10, name: '심소정', phonenumber: '010-123-4567', accepted: 'rejected' },
-// ];
